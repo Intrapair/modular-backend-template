3f3c0165b0defaacfc2e265f3f6cebddd3ba49e6
Toheeb Olayemi <olayemitoheeblekan@gmail.com>
2023-11-11 23:20:24 +0100

    update validation to use try..catch helper

--- src/middlewares/validations/user.validation.ts
+++ src/middlewares/validations/user.validation.ts
@@ -1,6 +1,8 @@
 import { ValidationError, body, validationResult } from 'express-validator';
 import { Request, Response, NextFunction } from 'express';
-import { errorResponse } from '../../utils/lib/response';
+import AppError from '../../utils/lib/appError';
+import { StatusCodes } from 'http-status-codes';
+import tryCatch from '../../utils/helpers/tryCatch.helper';
 
 const errorFormatter = ({ msg }: ValidationError) => {
     // Build your resulting errors however you want! String, object, whatever - it works!
@@ -14,26 +16,32 @@ const errorFormatter = ({ msg }: ValidationError) => {
  * @param next The next function
  * @returns ErrorResponse | NextFunction
  */
-export const loginValidator = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const emailCheck = body('email', 'Your email is not valid')
-        .isEmail()
-        .normalizeEmail()
-        .run(req);
-    const passwordCheck = body(
-        'password',
-        'Your password must be at least 4 characters'
-    )
-        .trim()
-        .isLength({ min: 4 })
-        .run(req);
-    await Promise.all([emailCheck, passwordCheck]);
-    const errors = validationResult(req).formatWith(errorFormatter);
-    if (!errors.isEmpty()) {
-        return errorResponse(res, errors.array().join(', '), 400);
+export const loginValidator = tryCatch(
+    async (req: Request, res: Response, next: NextFunction) => {
+        await Promise.all([
+            body('email')
+                .trim()
+                .notEmpty()
+                .withMessage('Email is required')
+                .isEmail()
+                .withMessage('Email is not valid')
+                .normalizeEmail()
+                .run(req),
+            body('password')
+                .trim()
+                .notEmpty()
+                .withMessage('Password is required')
+                .isLength({ min: 4 })
+                .withMessage('Password must be at least 4 characters')
+                .run(req),
+        ]);
+        const errors = validationResult(req).formatWith(errorFormatter);
+        if (!errors.isEmpty()) {
+            throw new AppError(
+                errors.array().join(', '),
+                StatusCodes.BAD_REQUEST
+            );
+        }
+        next();
     }
-    next();
-};
+);
